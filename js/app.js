e2fb6cb017a6b5e74e6b785a86ff2714ce73bede
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-10-28 00:14:39 +0100

    Changed action from prompt to Form

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
-setTimeout(function () {
+function compute(form) {
 
-    //Recives user input
-    var userInput = prompt("Enter the 🇳🇬 Telephone number without (+234) 😊");
+    var userInput = parseInt(form.telephone.value);
+    var userInput = Number(userInput);
 
     //Converts number to GSM acceptable codes
-    var checkNumber = userInput.slice(0, 4);
+    var checkNumber = userInput.toString().slice(0, 3);
+    var checkNumber = 0 + checkNumber;
 
     //Checks validity of number, (must start with 0)
-    var checkValid = userInput.slice(0, 1);
+    var checkValid = userInput.toString().slice(0);
+    var checkValid = 0 + checkValid;
 
     //Stores initial number
-    var iInput = userInput.slice(0);
+    var iInput = userInput.toString().slice(0);
+    var iInput = 0 + iInput;
+
+    console.log(checkValid)
 
     //Returns output of the converter
     function validate(checker) {
@@ -62,25 +67,25 @@ setTimeout(function () {
 
 
     //Validates number and detects network
-    if (userInput.length !== 11) {
-        alert("Invalid number, TRY AGAIN!!")
-    }
-    else if (checkValid !== ('0')) {
-        alert("Invalid number, number must start with '0', TRY AGAIN!!");
+    if (checkValid.length < 11 || checkValid.lenght > 11) {
+        alert("Error! Invalid number. Number must not be lesser or greater than 11")
     }
     else if (checkNumber === m1.GSM || checkNumber === m2.GSM || checkNumber === m3.GSM || checkNumber === m4.GSM || checkNumber === m5.GSM || checkNumber === m6.GSM || checkNumber === m7.GSM || checkNumber === m8.GSM || checkNumber === m9.GSM) {
-        alert(iInput + " belongs to MTN network 💛")
+        form.nettyResult.value = iInput + " belongs to MTN network 💛"
     }
     else if (checkNumber === g1.GSM || checkNumber === g2.GSM || checkNumber === g3.GSM || checkNumber === g4.GSM || checkNumber === g5.GSM || checkNumber === g6.GSM || checkNumber === g7.GSM) {
-        alert(iInput + " belongs to GLO network 💚");
+        form.nettyResult.value = iInput + " belongs to GLO network 💚"
     }
     else if (checkNumber === n1.GSM || checkNumber === n2.GSM || checkNumber === n3.GSM || checkNumber === n4.GSM) {
-        alert(iInput + " belongs to 9MOBILE network 💚");
+        form.nettyResult.value = iInput + " belongs to 9MOBILE network 💚"
     }
     else if (checkNumber === a1.GSM || checkNumber === a2.GSM || checkNumber === a3.GSM || checkNumber === a4.GSM || checkNumber === a5.GSM || checkNumber === a6.GSM) {
-        alert(iInput + " belongs to AIRTEL network ❤️");
+        form.nettyResult.value = iInput + " belongs to AIRTEL network 💛️"
     }
     else {
         alert(iInput + " belongs to no Nigerian network, check the number and TRY AGAIN!!")
     }
-}, 1800);
+}
+
+console.log("Hurray! Script working perfectly")
+console.log("Developed by Bolaji Ayodeji")
