5d12228e56b80c017af4bac4af0ce18438117b72
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-10-28 01:15:40 +0100

    Added support for more network providers

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ function compute(form) {
     var userInput = parseInt(form.telephone.value);
     var userInput = Number(userInput);
 
-    //Converts number to GSM acceptable codes
+    //Converts number to GSM acceptable Prefixes
     var checkNumber = userInput.toString().slice(0, 3);
     var checkNumber = 0 + checkNumber;
+
+    var checkNumber2 = userInput.toString().slice(0, 4);
+    var checkNumber2 = 0 + checkNumber2;
 
     //Checks validity of number, (must start with 0)
     var checkValid = userInput.toString().slice(0);
@@ -15,55 +18,82 @@ function compute(form) {
     var iInput = userInput.toString().slice(0);
     var iInput = 0 + iInput;
 
-    console.log(checkValid)
-
     //Returns output of the converter
-    function validate(checker) {
+    function validate(checker1) {
         return checkNumber;
+    }
+
+    function validate(checker2) {
+        return checkNumber2;
     }
 
     //Prepares input result
     var myResult = validate(userInput);
+    var myResult2 = validate(userInput);
 
 
-    //Stores GSM codes
-    var gCodes = function (GSM) {
+    //Stores GSM Prefixes
+    var gPrefixes = function (GSM) {
         this.GSM = GSM;
     }
 
-    //MTN GSM codes
-    var m1 = new gCodes('0806');
-    var m2 = new gCodes('0803');
-    var m3 = new gCodes('0816');
-    var m4 = new gCodes('0813');
-    var m5 = new gCodes('0810');
-    var m6 = new gCodes('0814');
-    var m7 = new gCodes('0816');
-    var m8 = new gCodes('0703');
-    var m9 = new gCodes('0706');
+    //MTN GSM Prefixes
+    var m1 = new gPrefixes('0806');
+    var m2 = new gPrefixes('0803');
+    var m3 = new gPrefixes('0816');
+    var m4 = new gPrefixes('0813');
+    var m5 = new gPrefixes('0810');
+    var m6 = new gPrefixes('0814');
+    var m7 = new gPrefixes('0903');
+    var m8 = new gPrefixes('0703');
+    var m9 = new gPrefixes('0706');
 
-    //GLO GSM codes
-    var g1 = new gCodes('0805');
-    var g2 = new gCodes('0705');
-    var g3 = new gCodes('0905');
-    var g4 = new gCodes('0807');
-    var g5 = new gCodes('0815');
-    var g6 = new gCodes('0811');
-    var g7 = new gCodes('0905');
+    //GLO GSM Prefixes
+    var g1 = new gPrefixes('0805');
+    var g2 = new gPrefixes('0705');
+    var g3 = new gPrefixes('0905');
+    var g4 = new gPrefixes('0807');
+    var g5 = new gPrefixes('0815');
+    var g6 = new gPrefixes('0811');
 
-    //9MOBILE GSM codes
-    var n1 = new gCodes('0809');
-    var n2 = new gCodes('0909');
-    var n3 = new gCodes('0817');
-    var n4 = new gCodes('0818');
+    //9MOBILE GSM Prefixes
+    var n1 = new gPrefixes('0809');
+    var n2 = new gPrefixes('0909');
+    var n3 = new gPrefixes('0817');
+    var n4 = new gPrefixes('0818');
+    var n5 = new gPrefixes('0908');
 
-    //AIRTEL GSM codes
-    var a1 = new gCodes('0802');
-    var a2 = new gCodes('0902');
-    var a3 = new gCodes('0701');
-    var a4 = new gCodes('0808');
-    var a5 = new gCodes('0708');
-    var a6 = new gCodes('0812');
+    //AIRTEL GSM Prefixes
+    var a1 = new gPrefixes('0802');
+    var a2 = new gPrefixes('0902');
+    var a3 = new gPrefixes('0701');
+    var a4 = new gPrefixes('0808');
+    var a5 = new gPrefixes('0708');
+    var a6 = new gPrefixes('0812');
+    var a7 = new gPrefixes('0907');
+
+    //STARCOMMS GSM Prefixes
+    var s1 = new gPrefixes('07028');
+    var s2 = new gPrefixes('07029');
+    var s3 = new gPrefixes('0819');
+
+    //VISAFONE GSM Prefixes
+    var v1 = new gPrefixes('07025');
+    var v2 = new gPrefixes('07026');
+    var v3 = new gPrefixes('0704');
+
+    //MULTILINKS GSM Prefixes
+    var mu1 = new gPrefixes('07027');
+    var mu2 = new gPrefixes('0709');
+
+    //ZOOM GSM Prefix
+    var z1 = new gPrefixes('0707')
+
+    //NTEL GSM Prefix
+    var nt1 = new gPrefixes('0804')
+
+    //SMILE GSM Prefix
+    var sm1 = new gPrefixes('0702')
 
 
     //Validates number and detects network
@@ -73,14 +103,35 @@ function compute(form) {
     else if (checkNumber === m1.GSM || checkNumber === m2.GSM || checkNumber === m3.GSM || checkNumber === m4.GSM || checkNumber === m5.GSM || checkNumber === m6.GSM || checkNumber === m7.GSM || checkNumber === m8.GSM || checkNumber === m9.GSM) {
         form.nettyResult.value = iInput + " belongs to MTN network 💛"
     }
-    else if (checkNumber === g1.GSM || checkNumber === g2.GSM || checkNumber === g3.GSM || checkNumber === g4.GSM || checkNumber === g5.GSM || checkNumber === g6.GSM || checkNumber === g7.GSM) {
+    else if (checkNumber === g1.GSM || checkNumber === g2.GSM || checkNumber === g3.GSM || checkNumber === g4.GSM || checkNumber === g5.GSM || checkNumber === g6.GSM) {
         form.nettyResult.value = iInput + " belongs to GLO network 💚"
     }
-    else if (checkNumber === n1.GSM || checkNumber === n2.GSM || checkNumber === n3.GSM || checkNumber === n4.GSM) {
+    else if (checkNumber === n1.GSM || checkNumber === n2.GSM || checkNumber === n3.GSM || checkNumber === n4.GSM || checkNumber === n5.GSM) {
         form.nettyResult.value = iInput + " belongs to 9MOBILE network 💚"
     }
-    else if (checkNumber === a1.GSM || checkNumber === a2.GSM || checkNumber === a3.GSM || checkNumber === a4.GSM || checkNumber === a5.GSM || checkNumber === a6.GSM) {
-        form.nettyResult.value = iInput + " belongs to AIRTEL network 💛️"
+    else if (checkNumber === a1.GSM || checkNumber === a2.GSM || checkNumber === a3.GSM || checkNumber === a4.GSM || checkNumber === a5.GSM || checkNumber === a6.GSM || checkNumber === a7.GSM) {
+        form.nettyResult.value = iInput + " belongs to AIRTEL network ❤️"
+    }
+    else if (checkNumber2 === s1.GSM || checkNumber2 === s2.GSM || checkNumber === s3.GSM) {
+        form.nettyResult.value = iInput + " belongs to STARCOMMS network 💛️"
+    }
+    else if (checkNumber2 === v1.GSM || checkNumber2 === v2.GSM || checkNumber === v3.GSM) {
+        form.nettyResult.value = iInput + " belongs to VISAFONE network 💗"
+    }
+    else if (checkNumber2 === mu1.GSM || checkNumber === mu2.GSM) {
+        form.nettyResult.value = iInput + " belongs to MULTILINKS network 💙"
+    }
+    else if (checkNumber === s1.GSM || checkNumber === s2.GSM || checkNumber === s3.GSM) {
+        form.nettyResult.value = iInput + " belongs to STARCOMMS network 💛️"
+    }
+    else if (checkNumber === z1.GSM) {
+        form.nettyResult.value = iInput + " belongs to ZOOM network 💛️"
+    }
+    else if (checkNumber === nt1.GSM) {
+        form.nettyResult.value = iInput + " belongs to NTEL network 💖"
+    }
+    else if (checkNumber === sm1.GSM) {
+        form.nettyResult.value = iInput + " belongs to SMILE network 💛️"
     }
     else {
         alert(iInput + " belongs to no Nigerian network, check the number and TRY AGAIN!!")
