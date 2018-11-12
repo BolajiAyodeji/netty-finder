dcd316a5fa414207079db1680671098535680bb9
Oyedele Hammed <itz.harmid@gmail.com>
2018-11-12 15:38:49 +0100

    Changed formatting!

--- js/app.js
+++ js/app.js
@@ -1,67 +1,67 @@
-( function ( $ ) {
+(function ($) {
 
   "use strict";
 
   // WRAP INTRO TEXT
-  $( '.ml1 .letters' ).each( function () {
-    $( this ).html( $( this ).text().replace( /([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>" ) );
-  } );
+  $('.ml1 .letters').each(function () {
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
 
-  anime.timeline( {
+  anime.timeline({
       loop: true
-    } )
-    .add( {
+    })
+    .add({
       targets: '.ml1 .letter',
       scale: [ 0.3, 1 ],
       opacity: [ 0, 1 ],
       translateZ: 0,
       easing: "easeOutExpo",
       duration: 100,
-      delay: function ( el, i ) {
-        return 70 * ( i + 1 )
+      delay: function(_, i) {
+        return 70 * (i + 1)
       },
-    } ).add( {
+    } ).add({
       targets: '.ml1 .line',
       scaleX: [ 0, 1 ],
       opacity: [ 0.5, 1 ],
       easing: "easeOutExpo",
       duration: 900,
       offset: '-=875',
-      delay: function ( el, i, l ) {
-        return 80 * ( l - i );
+      delay: function (_, i, l) {
+        return 80 * (l - i);
       },
-    } ).add( {
+    }).add({
       targets: '.ml1',
       opacity: 0,
       duration: 1000000000,
       easing: "easeOutExpo",
       delay: 1000
-    } );
+    });
 
-} )( jQuery );
+})(jQuery);
 
-const compute = function( ev ) {
-
+const compute = function(ev) {
   ev.preventDefault();
 
   let form = ev.target;
   let phone = form.telephone.value;
-  let detector = new NetworkDetect( phone );
+  let detector = new NetworkDetect(phone);
   let result = form.nettyResult;
 
   try {
     let network = detector.getNetworkName();
 
-    if ( !network ) {
+    if (! network) {
       return result.value = "🚫 " + this.phone + " belongs to no Nigerian network, check the number and TRY AGAIN!!";
     }
 
     form.telephone.value = ''; // clear input field
     return result.value = phone + ' belongs to the ' + network + ' network 💚';
-  } catch ( e ) {
-    return alert( e.message )
+  } catch (e) {
+    return alert(e.message);
   }
+
 }
 
-console.log( "Hurray! Script working perfectly" )
-console.log( "Developed by Bolaji Ayodeji" )
+console.log("Hurray! Script working perfectly")
+console.log("Developed by Bolaji Ayodeji")
