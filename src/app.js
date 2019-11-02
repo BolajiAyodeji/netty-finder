f43e9a92a0edc3751566e00e191c00649652f4cf
bolajiayodeji <sirbeejay1@gmail.com>
2019-11-02 15:23:52 +0100

    clean up UI

--- src/app.js
+++ src/app.js
@@ -1,45 +1,3 @@
-(function ($) {
-
-  "use strict";
-
-  // WRAP INTRO TEXT
-  $('.ml1 .letters').each(function () {
-    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-  });
-
-  anime.timeline({
-      loop: true
-    })
-    .add({
-      targets: '.ml1 .letter',
-      scale: [ 0.3, 1 ],
-      opacity: [ 0, 1 ],
-      translateZ: 0,
-      easing: "easeOutExpo",
-      duration: 100,
-      delay: function(_, i) {
-        return 70 * (i + 1)
-      },
-    } ).add({
-      targets: '.ml1 .line',
-      scaleX: [ 0, 1 ],
-      opacity: [ 0.5, 1 ],
-      easing: "easeOutExpo",
-      duration: 900,
-      offset: '-=875',
-      delay: function (_, i, l) {
-        return 80 * (l - i);
-      },
-    }).add({
-      targets: '.ml1',
-      opacity: 0,
-      duration: 1000000000,
-      easing: "easeOutExpo",
-      delay: 1000
-    });
-
-})(jQuery);
-
 const compute = function(ev) {
   ev.preventDefault();
 
@@ -51,7 +9,7 @@ const compute = function(ev) {
   try {
     let network = detector.getNetworkName();
 
-    if (! network) {
+    if (!network) {
       return result.value = "🚫 " + this.phone + " belongs to no Nigerian network, check the number and TRY AGAIN!!";
     }
 
@@ -63,5 +21,4 @@ const compute = function(ev) {
 
 }
 
-console.log("Hurray! Script working perfectly")
 console.log("Developed by Bolaji Ayodeji")
