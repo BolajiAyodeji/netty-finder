ac37a2af0bac69a92eaa7e7a41183363096aa86e
Uk <ukchukundah@gmail.com>
2019-11-11 07:33:30 +0100

    Use regex instead of testing string inclusion.

--- src/network-detect.js
+++ src/network-detect.js
@@ -5,92 +5,37 @@ const NetworkDetect = function (phone) {
 NetworkDetect.prototype = {
   getNetworks: function () {
     return {
-      '9mobile': ['0809', '0909', '0817', '0818', '0908'],
-      mtn: ['0806', '0803', '0816', '0813', '0810', '0814', '0903', '0906', '0703', '0706',],
-      glo: ['0805', '0705', '0905', '0807', '0815', '0811'],
-      airtel: ['0802', '0902', '0701', '0808', '0708', '0812', '0907'],
-      starcomms: ['07028', '07029', '0819'],
-      visafone: ['07025', '07026', '0704'],
-      multilinks: ['07027', '0709'],
-      zoom: ['0707'],
-      ntel: ['0804'],
-      smile: ['0702'],
+      '9mobile': /^0(8(09|17|18)|90(8|9))/,
+      MTN: /^0((7|8|9)0(3|6)|81(0|3|6|4))/,
+      GLO: /^0(705|8(05|07|11|15)|905)/,
+      Airtel: /^0(70(1|8)|8(02|08|12)|90(2|7))/,
+      Starcomms: /^0(702(8|9)|819)/,
+      Visafone: /^070(25|26|4)/,
+      Multilinks: /^070(27|9)/,
+      Zoom: /^0707/,
+      Ntel: /^0804/,
+      Smile: /^0702/,
     }
   },
 
   getNetworkName: function () {
     this.validate()
 
-    let phone_primary_prefix = this.getPhonePrefix()
-    let phone_secondary_prefix = this.getPhonePrefix(5)
-    let networks = this.getNetworks()
+    // Find the first entry that matches the phone
+    const [network, _] = Object.entries(this.getNetworks()).find(([_, regex]) => regex.test(this.phone)) || [null]
 
-    if (networks.mtn.includes(phone_primary_prefix)) {
-      return 'MTN'
-    }
-
-    if (networks.glo.includes(phone_primary_prefix)) {
-      return 'GLO'
-    }
-
-    if (networks['9mobile'].includes(phone_primary_prefix)) {
-      return '9mobile'
-    }
-
-    if (networks.airtel.includes(phone_primary_prefix)) {
-      return 'Airtel'
-    }
-
-    if (
-      networks.starcomms.includes(phone_secondary_prefix) ||
-      networks.starcomms.includes(phone_primary_prefix)
-    ) {
-      return 'Starcomms'
-    }
-
-    if (
-      networks.visafone.includes(phone_secondary_prefix) ||
-      networks.visafone.includes(phone_primary_prefix)
-    ) {
-      return 'Visafone'
-    }
-
-    if (
-      networks.multilinks.includes(phone_primary_prefix) ||
-      networks.multilinks.includes(phone_secondary_prefix)
-    ) {
-      return 'Multilinks'
-    }
-
-    if (networks.zoom.includes(phone_primary_prefix)) {
-      return 'Zoom'
-    }
-
-    if (networks.ntel.includes(phone_primary_prefix)) {
-      return 'Ntel'
-    }
-
-    if (networks.smile.includes(phone_primary_prefix)) {
-      return 'Smile'
-    }
-
-    return null
-  },
-
-  getPhonePrefix: function (length) {
-    length = length || 4
-    return this.phone.substr(0, length)
+    return network
   },
 
   validate: function () {
-    //Check if no value is passed
-    if (!this.phone) {
+    //Check that phone is a string
+    if (typeof this.phone !== 'string') {
       throw new Error('Invalid entry, enter a telephone number')
     }
 
-    //Check if number contains unwanted characters
-    if (this.phone.match(/[^0-9]/)) {
-      throw new Error('Number contains unwanted characters')
+    //Check that phone contains only digits
+    if (!/^\d+$/.test(this.phone)) {
+      throw new Error('Number contains non-digits')
     }
 
     //Check if number is lesser than 11
